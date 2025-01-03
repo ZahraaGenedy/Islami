67e3885414328f8637b2f0bfe75590d8b1eddd01
ZahraaGenedy <zahraagenedy151@gmail.com>
2025-01-03 17:20:37 +0300

    create sura content details

--- lib/screens/quranTap/suraDetails.dart
+++ lib/screens/quranTap/suraDetails.dart
@@ -19,10 +19,12 @@ class _SuradetailsState extends State<Suradetails> {
   @override
   Widget build(BuildContext context) {
     var arg = ModalRoute.of(context)!.settings.arguments as QuranDetailsModel;
-    if (verses.isEmpty) getFileData(arg.index);
-    if (suraContentjoin.isEmpty) getFileData(arg.index);
+    if (verses.isEmpty && suraContentjoin.isEmpty) {
+      getFileData(arg.index);
+    }
 
     return Scaffold(
+      backgroundColor: AppColors.black,
       appBar: AppBar(
         iconTheme: IconThemeData(color: Colors.white),
         backgroundColor: AppColors.black,
@@ -32,73 +34,90 @@ class _SuradetailsState extends State<Suradetails> {
         ),
         centerTitle: true,
       ),
-      body: Stack(alignment: Alignment.center, children: [
-        Container(
-          color: AppColors.black,
-          width: double.infinity,
-          height: double.infinity,
-          child: Image.asset(
-            "assets/images/detailsbg.png",
-            fit: BoxFit.fill,
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          Positioned.fill(
+            child: Image.asset(
+              "assets/images/detailsbg.png",
+              fit: BoxFit.fill,
+            ),
           ),
-        ),
-        Column(
-          children: [
-            Text(
-              "${arg.arabicQuranSuras}",
-              style: TextStyle(color: AppColors.primaryDark),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(18.0),
-              child: Text(
-                textAlign: TextAlign.center,
-                suraContentjoin,
-                style: TextStyle(
-                  color: AppColors.primaryDark,
+          Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(18.0),
+                child: Text(
+                  "${arg.arabicQuranSuras}",
+                  style: TextStyle(
+                    color: AppColors.primaryDark,
+                    fontSize: 24.0,
+                  ),
                 ),
-                textDirection: TextDirection.rtl,
-
               ),
-            ),
-            Expanded(
-              child: verses.isEmpty
-                  ? Center(
-                      child: CircularProgressIndicator(
-                        color: AppColors.primaryDark,
-                      ),
-                    )
-                  : ListView.builder(
-                      itemBuilder: (context, index) {
-                        return Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 20.0),
-                          child: Text(
-                            "${verses[index]}[${index + 1}]",
-                            style: TextStyle(color: AppColors.primaryDark),
-                            textDirection: TextDirection.rtl,
-                            textAlign: TextAlign.center,
-                          ),
-                        );
-                      },
-                      itemCount: verses.length,
+              Expanded(
+                child: SingleChildScrollView(
+                  padding: const EdgeInsets.all(18.0),
+                  child: Text(
+                    suraContentjoin,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: AppColors.primaryDark,
+                      fontSize: 18.0,
                     ),
-            ),
-          ],
-        )
-      ]),
+                    textDirection: TextDirection.rtl,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: verses.isEmpty
+                    ? Center(
+                  child: CircularProgressIndicator(
+                    color: AppColors.primaryDark,
+                  ),
+                )
+                    : ListView.builder(
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 6),
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 6),
+                        decoration: BoxDecoration(
+                          color: Colors.transparent,
+                          border: Border.all(color: AppColors.primaryDark),
+                          borderRadius: BorderRadius.circular(5)
+                        ),
+                        child: Text(
+                          "${verses[index]}[${index + 1}]",
+                          style: TextStyle(color: AppColors.primaryDark),
+                          textDirection: TextDirection.rtl,
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    );
+                  },
+                  itemCount: verses.length,
+                ),
+              ),
+            ],
+          ),
+
+        ]),
     );
   }
 
   void getFileData(int index) async {
-    String content =
-        await rootBundle.loadString("assets/files/${index + 1}.txt");
+    String content = await rootBundle.loadString("assets/files/${index + 1}.txt");
     List<String> suraLine = content.split("\n");
     String joinedContent = suraLine
         .asMap()
         .entries
         .map((entry) => "${entry.value}[${entry.key + 1}]")
         .join(" ");
-    verses = suraLine;
-    suraContentjoin = joinedContent;
-    setState(() {});
+
+    setState(() {
+      verses = suraLine;
+      suraContentjoin = joinedContent;
+    });
   }
 }
